41a41684c6b49e5b7a237f34ad4d0711189377d9
Martin Oswald <73196164+giraffacarp@users.noreply.github.com>
2025-05-21 08:04:49 +0200

    blink add enter preset

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,6 +5,7 @@ local M = {
 
 M.opts = function(_, opts)
   opts.keymap = {
+    preset = "enter",
     ["<C-j>"] = { "select_next", "fallback" },
     ["<C-k>"] = { "select_prev", "fallback" },
   }
